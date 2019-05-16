af3de6aa09981fa481f7ca387a034d9594bf0dc6
Alexander Demidenko <alexfoxref@gmail.com>
2019-05-16 20:46:08 +0500

    tests

---
+++ script.js
@@ -0,0 +1,65 @@
+// Функция sum должна возвращать тип данных true. Проверить её на это.
+
+
+function sum(a, b) {
+	return a + b > 10;
+}
+sum(2,2)
+
+let assert = require('chai').assert;
+
+describe('sum', function() {
+
+    it('сумма - true', function() {
+        assert.isTrue(sum(2, 2), 'неправда')
+    });
+});
+
+// Переменная num должна быть равна 5. Проверить на соответствие.
+
+
+let arr = [ [1, 2, 3], [4, 5, 6], [7,8,9] ];
+let num = arr[1][1];
+
+
+describe('num', function() {
+
+    it('переменная равна 5', function() {
+        assert.equal(num, 5, 'переменная не равна 5')
+    });
+});
+
+// Узнать, что нам вернет функция each в данных условиях. Проверить её на тип данных, который она возвращает,
+// на соответствие ожидаемому результату (который вы получили) и на свойство length.
+
+var each = function(startArr, f){return f(startArr)};
+var array = [64, 49, 36, 25, 16];
+var myFunc = function(a){
+	var newArr = [];
+	for (var i = 0; i < a.length; i++) {
+		newArr[i]=Math.sqrt(a[i]);
+	}
+	return newArr;
+}
+console.log(each(array, myFunc));
+
+describe('each', function() {
+
+    it('тип данных - массив', function() {
+        assert.isArray(each(array, myFunc), 'не массив')
+	});
+	it('ответ: [8,7,6,5,4]', function() {
+        assert.sameMembers(each(array, myFunc), [8, 7, 6, 5, 4], 'ответ не равен [8,7,6,5,4]')
+	});
+	it('длина массива равна 5', function() {
+        assert.lengthOf(each(array, myFunc), 5, 'размер массива не 5')
+    });
+});
+
+module.exports = {
+	sum: sum,
+	num: num,
+	arr: array,
+	func: myFunc,
+	each: each
+}
---
+++ test.js
@@ -0,0 +1,30 @@
+const res = require('./script.js');
+
+describe('sum', () => {
+
+    it(`sum - это правда`, () => {
+        expect(res.sum(2, 2)).toBeTruthy()
+    })
+})
+
+describe('num', () => {
+
+    it(`num равен 5`, () => {
+        expect(res.num).toBe(5)
+    })
+})
+
+describe('each', function() {
+    const result = res.each(res.arr, res.func);
+
+    it('тип данных ответа - объект', function() {
+        expect(typeof(result)).toStrictEqual('object')
+    });
+    it('ответ равен [8,7,6,5,4]', function() {
+        expect(result).toEqual([8,7,6,5,4])
+    });
+    it('размер массива равен 5', function() {
+        expect(result).toHaveLength(5)
+	});
+
+});
